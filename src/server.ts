22fc61e4e21118f94d812e4d8e8e054685b9700f
farkhanmhd <farkhanmuhammadv3@gmail.com>
2024-04-07 00:34:16 +0700

    change port

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import routes from './routes';
 
 const init = async () => {
   const server = Hapi.server({
-    port: 3000,
+    port: 5000,
     host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     routes: {
       cors: {
